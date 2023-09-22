d69958f637da293293c91ffede6c45a5cee74696
chimeikem-github <collins.elom47@gmail.com>
2023-09-22 21:52:00 +0100

    Data model added

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+    
+    def __str__(self):
+        return self.name;
